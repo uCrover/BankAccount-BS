5f0247a8a9e8ae7994120385d30fcb972834e1f0
uCrover <chris66xd@gmail.com>
2022-02-14 11:50:31 -0500

    dependencias 1

---
+++ src/main/java/com/bootcamp/bank_accountservice/BankAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bank_accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/bank_accountservice/BankAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bank_accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
